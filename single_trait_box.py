1f964f9c2597ffa6d2b5c77c299a36f75d7f68f5
Andrew Sayman <lorien@localhost.localdomain>
2007-07-26 23:35:54 -0400

    Forgot to add SingleTraitBox in all of that hubbub

---
+++ single_trait_box.py
@@ -0,0 +1,83 @@
+import gtk
+import gtk.glade
+import gobject
+
+class SingleTraitBox:
+	# Trait columns
+	(
+		COLUMN_NAME,
+		COLUMN_VALUE,
+		COLUMN_NOTE
+	) = range(3)
+	__traitbox_xml_file  = '/home/lorien/tmp/crapvine/interface/TraitBox.glade'
+	def __init__(self, trait_menu_name, trait_display_name, overlord):
+		self.xml = gtk.glade.XML(self.__traitbox_xml_file, 'traitbox')
+		self.vbox = self.xml.get_widget('traitbox')
+		self.title = self.xml.get_widget('lblTraitBoxTitle')
+		self.tree = self.xml.get_widget('treeTraits')
+		self.trait_menu_name = trait_menu_name
+		self.trait_display_name = trait_display_name
+		self.overlord = overlord
+
+		model = self.__create_available_traits_model()
+		self.tree.set_model(model)
+
+		renderer = gtk.CellRendererText()
+		renderer.set_data("column", self.COLUMN_NAME)
+		column = gtk.TreeViewColumn("Name", renderer, text=self.COLUMN_NAME)
+		self.tree.append_column(column)
+
+		renderer = gtk.CellRendererText()
+		renderer.set_data("column", self.COLUMN_VALUE)
+		column = gtk.TreeViewColumn("Value", renderer, text=self.COLUMN_VALUE)
+		self.tree.append_column(column)
+
+		renderer = gtk.CellRendererText()
+		renderer.set_data("column", self.COLUMN_NOTE)
+		column = gtk.TreeViewColumn("Note", renderer, text=self.COLUMN_NOTE)
+		self.tree.append_column(column)
+
+		self.tree.connect('cursor-changed', self.set_traitbox_focus)
+
+		self.title.set_label(self.trait_display_name)
+
+		self.xml.signal_autoconnect({
+			'on_add_to_trait': self.on_add_to_trait,
+			'on_subtract_from_trait': self.on_subtract_from_trait,
+			'set_traitbox_focus': self.set_traitbox_focus,
+			'on_scrolledwindow3_set_focus_child': self.set_focus_child,
+			'on_treeTraits_set_focus_child': self.set_focus_child,
+			'on_row_activated': self.on_row_activated
+		})
+
+	def on_add_to_trait(self, widget):
+		print "Adding trait on %s" % self.trait_menu_name
+
+	def on_subtract_from_trait(self, widget):
+		print "Subtracting trait from %s" % self.trait_menu_name
+
+	def set_focus_child(self, unused, unused_as_well):
+		self.set_traitbox_focus(None)
+
+	def set_traitbox_focus(self, unused):
+		print "Setting traitbox focus"
+		self.overlord.target_traitbox = self
+		self.overlord.show_menu(self.trait_menu_name)
+
+	def get_vbox(self):
+		return self.vbox
+
+	def on_row_activated(self, treeview, path, view_column):
+		print "Row activated"
+		print path
+		row_num = path[0]
+		print row_num
+		print view_column
+
+	def __create_available_traits_model(self):
+		model = gtk.ListStore(
+			gobject.TYPE_STRING,
+			gobject.TYPE_STRING,
+			gobject.TYPE_STRING
+		)
+		return model
