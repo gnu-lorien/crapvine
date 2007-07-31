0a9a235a6b4df289f66135c80cd69284512e7f18
Andrew Sayman <lorien420@myrealbox.com>
2007-07-31 00:27:59 -0400

    Read vampire gex files
    This builds the structure to read them. Currently there is no display.
    
    It only handles vampires, traitlists, and traits.

--- grapevine_xml.py
+++ grapevine_xml.py
@@ -2,7 +2,7 @@ class AttributeReader:
 	def __init__(self, attrs):
 		self.attrs = attrs
 
-	def boolean(self, name):
+	def boolean(self, name, default=False):
 		if self.attrs.has_key(name):
 			bool_val = self.attrs.get(name)
 			if bool_val == 'yes':
@@ -12,9 +12,58 @@ class AttributeReader:
 			else:
 				raise 'Boolean xml field set to invalid value |%s|' % (bool_val)
 		return False
+	def b(self, name, default=False):
+		return self.boolean(name, default)
 
-	def text(self, name, default):
+	def text(self, name, default=''):
 		if self.attrs.has_key(name):
 			return self.attrs.get(name)
 		return default
-	
+	def t(self, name, default=''):
+		return self.text(name, default)
+
+	def number_as_text(self, name, default='0'):
+		# Should eventually verify that it's a number
+		return text(name, default)
+	def nat(self, name, default='0'):
+		return self.number_as_text(name, default)
+
+	def date(self, name, default='unknown'):
+		# Should eventually parse the date?
+		return text(name, default)
+	def d(self, name, default='unknown'):
+		return self.date(name, default)
+
+class Attributed(object):
+	def read_attributes(self, attrs):
+		r = AttributeReader(attrs)
+		# Actually make these required...
+		for attr in self.required_attrs:
+			if attrs.has_key(attr):
+				self.__setattr__(attr, attrs.get(attr))
+		for attr in self.text_attrs:
+			if attrs.has_key(attr):
+				self.__setattr__(attr, attrs.get(attr))
+		for attr in self.number_as_text_attrs:
+			if attrs.has_key(attr):
+				self.__setattr__(attr, attrs.get(attr))
+		for attr in self.date_attrs:
+			if attrs.has_key(attr):
+				self.__setattr__(attr, attrs.get(attr))
+		for attr in self.bool_attrs:
+			self.__setattr__(attr, r.b(attr))
+
+	def __getattr__(self, name):
+		try:
+			return object.__getattr__(self, name)
+		except AttributeError, e:
+			if name in self.required_attrs:
+				raise
+			if name in self.text_attrs:
+				return '' 
+			if name in self.number_as_text_attrs:
+				return '0' 
+			if name in self.date_attrs:
+				return ''
+			if name in self.bool_attrs:
+				return 'no'
---
+++ vampire.py
@@ -0,0 +1,117 @@
+from xml.sax import ContentHandler
+import string
+from grapevine_xml import AttributeReader, Attributed
+from xml.sax import make_parser
+from xml.sax.handler import feature_namespaces
+
+def normalize_whitespace(text):
+    "Remove redundant whitespace from a string"
+    return ' '.join(text.split())
+
+class VampireLoader(ContentHandler):
+	def __init__(self):
+		self.vampires = {}
+		self.current_vampire = None
+		self.current_traitlist = None
+
+	def add_vampire(self, vamp):
+		self.vampires[vamp.name] = vamp
+
+	def startElement(self, name, attrs):
+		if name == 'vampire':
+			if not attrs.has_key('name'):
+				return
+			v = Vampire()
+			v.read_attributes(attrs)
+			self.current_vampire = v
+		else:
+			if not self.current_vampire:
+				return
+
+		if name == 'experience':
+			pass
+
+		if name == 'traitlist':
+			if self.current_traitlist:
+				raise 'TraitList encountered while still reading traitlist'
+			tl = TraitList()
+			tl.read_attributes(attrs)
+			self.current_traitlist = tl
+			self.current_vampire.add_traitlist(tl)
+
+		if name == 'trait':
+			if not self.current_traitlist:
+				raise 'Trait without bounding traitlist'
+			t = Trait()
+			t.read_attributes(attrs)
+			self.current_traitlist.add_trait(t)
+
+	def endElement(self, name):
+		if name == 'vampire':
+			self.add_vampire(self.current_vampire)
+			self.current_vampire = None
+		elif name == 'traitlist':
+			self.current_traitlist = None
+
+class TraitList(Attributed):
+	required_attrs = ['name']
+	text_attrs = []
+	number_as_text_attrs = ['display']
+	date_attrs = []
+	bool_attrs = ['abc', 'atomic']
+
+	def __init__(self):
+		self.traits = []
+
+	def add_trait(self, trait):
+		self.traits.append(trait)
+
+	def __str__(self):
+		end_tag = ">\n" if len(self.traits) > 0 else "/>"
+		ret = '<traitlist name="%s" abc="%s" atomic="%s" display="%s" %s' % (self.name, self.abc, self.atomic, self.display, end_tag)
+		ret += "\n".join([str(trait) for trait in self.traits])
+		if len(self.traits) > 0:
+			ret += '</traitlist>'
+		return ret
+
+class Trait(Attributed):
+	required_attrs = ['name']
+	text_attrs = ['note']
+	number_as_text_attrs = ['val']
+	date_attrs = []
+	bool_attrs = []
+
+	def __str__(self):
+		return '<trait name="%s" val="%s" note="%s" />' % (self.name, self.val, self.note)
+
+class Vampire(Attributed):
+	required_attrs = ['name']
+	text_attrs = ['nature', 'demeanor', 'clan', 'sect', 'path', 'status', 'narrator']
+	number_as_text_attrs = ['generation', 'blood', 'willpower', 'conscience', 'selfcontrol', 'courage', 'pathtraits', 'physicalmax']
+	date_attrs = ['startdate', 'lastmodified']
+	bool_attrs = ['npc']
+
+	def __init__(self):
+		self.traitlists = []
+
+	def add_traitlist(self, traitlist):
+		self.traitlists.append(traitlist)
+
+	def ballz(self):
+		print self.__dict__
+
+	def __str__(self):
+		ret = '<vampire name="%s" nature="%s" demeanor="%s" clan="%s" sect="%s" generation="%s" blood="%s" willpower="%s" conscience="%s" selfcontrol="%s" courage="%s" path="%s" pathtraits="%s" physicalmax="%s" status="%s" startdate="%s" narrator="%s" npc="%s" lastmodified="%s">' % (self.name, self.nature, self.demeanor, self.clan, self.sect, self.generation, self.blood, self.willpower, self.conscience, self.selfcontrol, self.courage, self.path, self.pathtraits, self.physicalmax, self.status, self.startdate, self.narrator, self.npc, self.lastmodified)
+		ret += "\n".join([str(traitlist) for traitlist in self.traitlists])
+		ret += "\n</vampire>"
+		return ret
+
+parser = make_parser()
+parser.setFeature(feature_namespaces, 0)
+loader = VampireLoader()
+parser.setContentHandler(loader)
+parser.parse('/home/lorien/tmp/crapvine/exchange_samples/vampires_sabbat.gex')
+
+vamp = loader.vampires['Pack-Dasher']
+
+print vamp
