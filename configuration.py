0f710e509b5e04f20bc01415055141d1ed130a3d
Andrew Sayman <lorien420@myrealbox.com>
2007-08-08 19:55:27 -0400

    Separate base interface path

--- configuration.py
+++ configuration.py
@@ -2,21 +2,23 @@ from os import path
 
 def get_base_file_path():
 	return '/home/lorien/tmp/crapvine'
+def get_base_interface_path():
+	return path.join(get_base_file_path(), 'interface')
 
 def get_traitlist_box_xml_file_path():
-	return path.join(get_base_file_path(), 'interface', 'TraitlistBox.glade')
+	return path.join(get_base_interface_path(), 'TraitlistBox.glade')
 
 def get_text_attribute_box_xml_file_path():
-	return path.join(get_base_file_path(), 'interface', 'TextAttributeBox.glade')
+	return path.join(get_base_interface_path(), 'TextAttributeBox.glade')
 
 def get_number_as_text_attribute_box_xml_file_path():
-	return path.join(get_base_file_path(), 'interface', 'NumberAsTextAttributeBox.glade')
+	return path.join(get_base_interface_path(), 'NumberAsTextAttributeBox.glade')
 
 def get_menu_file_path():
-	return path.join(get_base_file_path(), 'interface', 'menus.gvm')
+	return path.join(get_base_interface_path(), 'menus.gvm')
 
 def get_character_tree_xml_file_path():
-	return path.join(get_base_file_path(), 'interface', 'CharacterTree.glade')
+	return path.join(get_base_interface_path(), 'CharacterTree.glade')
 
 def get_grapevine_xml_file_path():
-	return path.join(get_base_file_path(), 'interface', 'Grapevine.glade')
+	return path.join(get_base_interface_path(), 'Grapevine.glade')
