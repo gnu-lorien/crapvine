2de6c4e1f2b77bb99c692d14a2697f3d5e82681e
Andrew Sayman <lorien420@myrealbox.com>
2007-09-06 06:30:23 -0400

    Support building distributal versions of crapvine

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (name='crapvine',
+      version='0.0.1',
+      description='Python implementation of Grapevine',
+      author='Andrew Sayman',
+      author_email='lorien420@myrealbox.com',
+      url='http://repo.or.cz/w/crapvine.git',
+      license='GPLv3',
+      packages=['crapvine'],
+      package_dir={'crapvine': '.'},
+      package_data={'crapvine': ['exchange_samples/*', 'interface/*']}
+)
