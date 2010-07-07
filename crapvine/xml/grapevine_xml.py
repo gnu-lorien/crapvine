6c5372ca9430c4f4a21a5d20c357364d9ffec977
Andrew Sayman <andrew.sayman@gmail.com>
2010-07-07 16:01:38 -0400

    Remove unused attribute reader

--- crapvine/xml/grapevine_xml.py
+++ crapvine/xml/grapevine_xml.py
@@ -25,46 +25,9 @@ from datetime import datetime
 
 from attribute import AttributeBuilder, classmaker
 
-class AttributeReader:
-	def __init__(self, attrs):
-		self.attrs = attrs
-
-	def boolean(self, name, default=False):
-		if self.attrs.has_key(name):
-			bool_val = self.attrs.get(name)
-			if bool_val == 'yes':
-				return True
-			elif bool_val == 'no':
-				return False
-			else:
-				raise AttributeError('Boolean xml field set to invalid value |%s|' % (bool_val))
-		return False
-	def b(self, name, default=False):
-		return self.boolean(name, default)
-
-	def text(self, name, default=''):
-		if self.attrs.has_key(name):
-			return self.attrs.get(name)
-		return default
-	def t(self, name, default=''):
-		return self.text(name, default)
-
-	def number_as_text(self, name, default='0'):
-		# Should eventually verify that it's a number
-		return text(name, default)
-	def nat(self, name, default='0'):
-		return self.number_as_text(name, default)
-
-	def date(self, name, default='unknown'):
-		# Should eventually parse the date?
-		return text(name, default)
-	def d(self, name, default='unknown'):
-		return self.date(name, default)
-
 class Attributed(object):
 	__metaclass__ = AttributeBuilder
 	def read_attributes(self, attrs):
-		r = AttributeReader(attrs)
 		for attr in attrs.keys():
 			if hasattr(self, attr):
 				setattr(self, attr, unescape(attrs.get(attr)))
